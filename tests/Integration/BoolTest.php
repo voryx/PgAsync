fe002ff346d59a851453abf569edbf56fe1d70c8
matt <matt@bonneau.net>
2015-11-02 05:35:47 -0500

    Test for bool handling

---
+++ tests/Integration/BoolTest.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace PgAsync\Tests\Integration;
+
+
+use PgAsync\Client;
+use Rx\Observer\CallbackObserver;
+
+class BoolTest extends TestCase
+{
+    public function testBools() {
+        $conn = $this->getLoop();
+
+        $client = new Client(["user" => $this::getDbUser(), "database" => $this::getDbName()], $this->getLoop());
+
+        $count = $client->query("SELECT * FROM thing");
+
+        $trueCount = 0;
+        $falseCount = 0;
+        $nullCount = 0;
+        $completes = false;
+        $count->subscribe(new CallbackObserver(
+            function ($x) use (&$trueCount, &$falseCount, &$nullCount) {
+                if ($x['thing_in_stock'] === true) {
+                    $trueCount++;
+                }
+                if ($x['thing_in_stock'] === false) {
+                    $falseCount++;
+                }
+                if ($x['thing_in_stock'] === null) {
+                    $nullCount++;
+                }
+            },
+            function ($e) use ($client) {
+                $client->closeNow();
+                $this->cancelCurrentTimeoutTimer();
+                throw $e;
+            },
+            function () use (&$completes, $client) {
+                $completes = true;
+                $client->closeNow();
+                $this->cancelCurrentTimeoutTimer();
+            }
+        ));
+
+        $this->runLoopWithTimeout(2);
+
+        $client->closeNow();
+
+        $this->assertTrue($completes);
+        $this->assertEquals(1, $trueCount);
+        $this->assertEquals(1, $falseCount);
+        $this->assertEquals(1, $nullCount);
+    }
+
+}
